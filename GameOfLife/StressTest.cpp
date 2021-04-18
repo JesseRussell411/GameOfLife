8a72bceeb94dd1dcc77ff4e13e5157fb994c6876
techClubMember <jesserussell411@gmail.com>
2021-04-17 19:28:09 -0600

    Commented out stressTest.cpp

--- GameOfLife/StressTest.cpp
+++ GameOfLife/StressTest.cpp
@@ -1,20 +1,20 @@
-#include "GameOfLife.h"
-#include "Stopwatch.h"
-#include <iostream>
-#include<string>
-using namespace JesseRussell::Diagnostics;
-
-
-GameOfLife game(10000, 50000);
-int main() {
-	game.scramble();
-	Stopwatch watch;
-	std::cout << game.getThreadCount() << "\n";
-
-	while (true) {
-		watch.restart();
-		game.step();
-		watch.stop();
-		std::cout << watch.getElapsedMilliseconds() << "\n";
-	}
-}
+//#include "GameOfLife.h"
+//#include "Stopwatch.h"
+//#include <iostream>
+//#include<string>
+//using namespace JesseRussell::Diagnostics;
+//
+//
+//GameOfLife game(10000, 50000);
+//int main() {
+//	game.scramble();
+//	Stopwatch watch;
+//	std::cout << game.getThreadCount() << "\n";
+//
+//	while (true) {
+//		watch.restart();
+//		game.step();
+//		watch.stop();
+//		std::cout << watch.getElapsedMilliseconds() << "\n";
+//	}
+//}
